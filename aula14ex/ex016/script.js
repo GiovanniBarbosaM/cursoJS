a8a817295ff3b1ffd94abaada13a20ec622ff542
Giovanni Barbosa <giovannibarbosa.m@hotmail.com>
2025-07-26 08:07:35 -0300

    js at 26/07 - 01

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,10 @@
-alert('Olá, Mundo!');
+function contar() {
+    let ini = document.getElementById('txti').value;
+    let fim = document.getElementById('txtf').value;
+    let passo = document.getElementById('txtp').value;
+    let res = document.getElementById('res');
+
+    if ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 {
+        window.alert('[ERRO] Faltam dados!');
+    } else {}
+}
